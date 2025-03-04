c16bb07da8dd8a80eff70c9ef43a526f30d540fd
VipulArora_2003 <vipularora9930@gmail.com>
2025-03-04 15:12:18 +0530

    [Vipul] Add: Implement FizzBuzz logic

---
+++ FizzBuzz.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive number: ");
+        int n = sc.nextInt();
+
+        String[] result = new String[n];
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result[i - 1] = "FizzBuzz";
+            } else if (i % 3 == 0) {
+                result[i - 1] = "Fizz";
+            } else if (i % 5 == 0) {
+                result[i - 1] = "Buzz";
+            } else {
+                result[i - 1] = String.valueOf(i);
+            }
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println("Position " + (i + 1) + " = " + result[i]);
+        }
+
+        sc.close();
+    }
+}
